9a43f77f02e8dfb3b2f282153712a4f5ffac723a
Kalyan Kumar Bandi <Kalyankumarbandi@eurofins.com>
2020-02-25 15:30:40 +0530

    file addition logic

--- src/main/java/com/example/demodocker/DemoController.java
+++ src/main/java/com/example/demodocker/DemoController.java
@@ -1,23 +1,21 @@
 package com.example.demodocker;
 
+import java.io.File;
 import java.util.UUID;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
 @RestController
-public class DemoController
-{
+public class DemoController {
 	public static String ROOT_PATH = "/var/ftp/application/pub/";
-	
-    @RequestMapping("/")    
-    public String home()
-    {
-    	return "Hello! This is a docker demo on openshift";
-    }
-    
-    @RequestMapping("/path")
+
+	@RequestMapping("/")
+	public String home() {
+		return "Hello! This is a docker demo on openshift";
+	}
+
+	@RequestMapping("/path")
     public Response getPath() {
     	
     	try {
@@ -25,6 +23,19 @@ public class DemoController
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
+    	
+    	for (int i=0; i<10000; i++) {
+    		
+    		Thread t = new Thread(new Runnable() {
+				@Override
+				public void run() {
+					File f = new File("test-" + UUID.randomUUID().toString() + ".txt" );
+				} 
+    		});
+    		t.start();
+    	}
+    	
+    	
     	
     	return new Response(ROOT_PATH + UUID.randomUUID().toString());
     }
